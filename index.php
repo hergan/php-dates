8f4fc1f43ac52dd78225429b7f802f608d37eb61
Jordan Haldane <haldajo@gmail.com>
2017-12-12 21:45:34 +0000

    Starting to flesh out funcs.

--- index.php
+++ index.php
@@ -15,52 +15,50 @@
 
 	//cacls New Years Day
 	$newYearDay = date('U', strtotime("first day of january"));
-	echo "New Years Day is: " . "$newYearDay" . "<br>" . "<br>";
-	
+
 	//calc good friday from easter day minus two days 
 	$goodFriday = date('U', easter_date() - 172800); 
-	echo "Good Friday is: " . "$goodFriday" . "<br>" . "<br>";
 
 	//cacls Memorial Day
 	$memorialDay = date('U', strtotime("last monday of may"));
-	echo "Memorial Day is: " . "$memorialDay" . "<br>" . "<br>";
 
 	//cacls Independence Day
 	$independenceDay = date('U', strtotime("july 4"));
-	echo "Independence Day is: " . "$independenceDay" . "<br>" . "<br>";
 
 	//cacls Labor Day
 	$laborDay = date('U', strtotime("first monday of september"));
-	echo "Labor Day is: " . "$laborDay" . "<br>" . "<br>";
 
 	//cacls Thanksgiving Day
 	$thanksgivingDay = date('U', strtotime("fourth thursday of november"));
-	echo "Thanksgiving Day is: " . "$thanksgivingDay" . "<br>" . "<br>";
 
 	//cacls Friday after Thanksgiving
 	$fridayAfterThanksgiving = $thanksgivingDay + 86400;
-	echo "The Friday after Thanksgiving Day is: " . "$fridayAfterThanksgiving" . "<br>" . "<br>";
 
 	//cacls Christmas Eve
 	$chirstmasEve = date('U', strtotime("december 24"));
-	echo "Chirstmas Eve is: " . "$chirstmasEve" . "<br>" . "<br>";
 
 	//cacls Christmas Day
 	$chirstmasDay = date('U', strtotime("december 25"));
-	echo "Chirstmas Day is: " . "$chirstmasDay" . "<br>" . "<br>";
 
-	$holidaysOff = array($newYearDay, $goodFriday, $memorialDay, $independenceDay, $laborDay, $thanksgivingDay, $fridayAfterThanksgiving, $chirstmasEve, $chirstmasDay);
-	$arrLength = count($holidaysOff);
+	//cacls Day after Christmas
+	$dayAfterChirstmas = date('U', strtotime("december 26"));
 
-	for ($i = 0; $i < $arrLength; $i++) { 
-		echo $holidaysOff[$i] . "<br>". "<br>";
+	$holidaysOff = array($newYearDay, $goodFriday, $memorialDay, $independenceDay, $laborDay, $thanksgivingDay, $fridayAfterThanksgiving, $chirstmasEve, $chirstmasDay, $dayAfterChirstmas);
+
+	//calculates second thurs in UNIX
+	$secondThurs = date('U', strtotime("second thursday of this month"));
+
+	// compares $secondThurs var to holidaysOff array 
+	if (array_search ($secondThurs, $holidaysOff))
+	{
+		echo "Office is closed";
+	}
+	else 
+	{
+		$formDate = date_create_from_format("U",$secondThurs);
+		echo date_format($formDate, "l, F j");
 	}
 
-
-	
-	//calculates second thurs in UNIX
-//	$secondThurs = date('U', strtotime("second thursday of this month"));
-//	echo "$secondThurs" . "<br>";
 
 ?>
 
